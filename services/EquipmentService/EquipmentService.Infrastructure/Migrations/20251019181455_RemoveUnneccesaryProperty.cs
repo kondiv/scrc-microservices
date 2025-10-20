d5b317fbd9e76dd05b3be2344d55e4fca944ab6b
kondiv <ilyakondrachin77@gmail.com>
2025-10-20 21:22:12 +0300

    migrations

---
+++ services/EquipmentService/EquipmentService.Infrastructure/Migrations/20251019181455_RemoveUnneccesaryProperty.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace EquipmentService.Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class RemoveUnneccesaryProperty : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "requires_special_training",
+                table: "equipment");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<bool>(
+                name: "requires_special_training",
+                table: "equipment",
+                type: "boolean",
+                nullable: false,
+                defaultValue: false);
+        }
+    }
+}
