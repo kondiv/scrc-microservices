5452bb422b9a49cbcbdf934136c8de582a584367
kondiv <ilyakondrachin77@gmail.com>
2025-10-21 14:52:32 +0300

    commit for merge changes

---
+++ services/SparePartsWarehouse/SparePartsWarehouse.Domain/Entities/CompatibleEquipment.cs
@@ -0,0 +1,19 @@
+﻿namespace SparePartsWarehouse.Domain.Entities;
+
+public sealed class CompatibleEquipment
+{
+    public int Id { get; init; }
+    public string Model { get; init; }
+    public int MinVoltage { get; init; }
+    public int MaxVoltage { get; init; }
+    public ICollection<SparePartCompatibleEquipment> SparePartCompatibleEquipments { get; private set; }
+
+    public CompatibleEquipment(int id, string model, int minVoltage, int maxVoltage)
+    {
+        Id = id;
+        Model = model;
+        MinVoltage = minVoltage;
+        MaxVoltage = maxVoltage;
+        SparePartCompatibleEquipments = new List<SparePartCompatibleEquipment>();
+    }
+}
---
+++ services/SparePartsWarehouse/SparePartsWarehouse.Infrastructure/Configurations/SparePartConfiguration.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using SparePartsWarehouse.Domain.Entities;
+
+namespace SparePartsWarehouse.Infrastructure.Configurations;
+
+public sealed class SparePartConfiguration : IEntityTypeConfiguration<SparePart> 
+{
+    public void Configure(EntityTypeBuilder<SparePart> builder)
+    {
+        builder.ToTable("spare_parts");
+        
+        builder.HasKey(x => x.Id);
+        
+        builder.Property(x => x.Id).ValueGeneratedNever().HasColumnName("id");
+        builder.Property(x => x.Model).HasMaxLength(128).HasColumnName("model");
+        builder.HasMany(x => x.SparePartCompatibleEquipments)
+            .WithOne(x => x.SparePart)
+            .HasForeignKey(x => x.SparePartId)
+            .OnDelete(DeleteBehavior.Cascade);
+    }
+}
